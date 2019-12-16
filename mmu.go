2b69f63e076cc09ff2cd5c620c12551a7fd65384
Nick Barth <nickbarth@live.com>
2019-12-16 15:10:31 -0700

    Update mmu.go

--- mmu.go
+++ mmu.go
@@ -3,33 +3,33 @@ package main
 import "io/ioutil"
 
 type MMU struct {
-  rom []byte
+  ram []byte
 }
 
 func NewMMU() *MMU {
   return &MMU{
-    rom: []byte{},
+    ram: []byte{},
   }
 }
 
 func (m *MMU) Load(file string) {
   dat, _ := ioutil.ReadFile(file)
-  m.rom = dat
+  m.ram = append(dat, make([]byte, 0xffff)...) // hack
 }
 
 func (m *MMU) Read(addr uint16) byte {
-  return m.rom[addr]
+  return m.ram[addr]
 }
 
 func (m *MMU) ReadWord(addr uint16) uint16 {
-  return uint16(m.rom[addr] << 4 | m.rom[addr+1])
+  return uint16(m.ram[addr]) | uint16(m.ram[addr+1]) << 8
 }
 
-func (m *MMU) Write(addr int16, val byte) {
-  m.rom[addr] = val
+func (m *MMU) Write(addr uint16, val byte) {
+  m.ram[addr] = val
 }
 
-func (m *MMU) WriteWord(addr int16, val uint16) {
-  m.rom[addr] = byte(val >> 4)
-  m.rom[addr+1] = byte(val & 0b0000_1111)
+func (m *MMU) WriteWord(addr uint16, val uint16) {
+  m.ram[addr] = byte(val >> 4)
+  m.ram[addr+1] = byte(val & 0b0000_1111)
 }
