1f189cc91ef104979ae7d5d34a5945a1b82d5cb8
Nick Barth <nickbarth@live.com>
2019-12-20 14:37:25 -0700

    updated output

--- cpu.go
+++ cpu.go
@@ -23,13 +23,12 @@ func NewCPU() *CPU {
 func (cpu *CPU) Step(n uint16) {
   opcode := cpu.mmu.Read(cpu.pc)
 
-  // fmt.Printf("-- 0x%0x -- \n", cpu.sp)
-  fmt.Printf("0x%0x\t", opcode)
+  fmt.Printf("0x%x: \t(0x%0x)\t", n, opcode)
 
   switch opcode {
   case 0x00:
     // nop
-    fmt.Printf("0x%0x:\tnop\n", n,)
+    fmt.Printf("nop")
     cpu.t += 4
     cpu.pc += 1;
 
@@ -42,21 +41,21 @@ func (cpu *CPU) Step(n uint16) {
     cpu.reg.SetFlag('n', true)
     cpu.reg.SetFlag('h', cpu.reg.b & 0x10 == 0x10)
 
-    fmt.Printf("0x%0x:\tdec b\n", n)
+    fmt.Printf("dec b")
     cpu.pc += 1
 
   case 0x06:
     // ld b nn
     cpu.t += 8
     cpu.reg.b = cpu.mmu.Read(cpu.pc + 1)
-    fmt.Printf("0x%0x:\tld b 0x%x\n", n, cpu.mmu.Read(cpu.pc + 1))
+    fmt.Printf("ld b 0x%x", cpu.mmu.Read(cpu.pc + 1))
     cpu.pc += 2
 
   case 0x08:
     // ld (nn) sp
     cpu.t += 8
     cpu.reg.c = cpu.mmu.Read(cpu.pc + 1)
-    fmt.Printf("0x%0x:\tld c 0x%x\n", n, cpu.mmu.Read(cpu.pc + 1))
+    fmt.Printf("ld c 0x%x", cpu.mmu.Read(cpu.pc + 1))
     cpu.pc += 2
 
   case 0x17:
@@ -67,35 +66,35 @@ func (cpu *CPU) Step(n uint16) {
     c := cpu.reg.GetFlagVal('c')
     cpu.reg.SetFlag('c', cpu.reg.a & (1 << 7) >> 7 == 1)
     cpu.reg.a = cpu.reg.a << 1 | c
-    fmt.Printf("0x%0x:\trla\n", n)
+    fmt.Printf("rla")
     cpu.pc += 1
 
   case 0x0c:
     // inc c
     cpu.t += 4
     cpu.reg.c += 1
-    fmt.Printf("0x%0x:\tinc c\n", n)
+    fmt.Printf("inc c")
     cpu.pc += 1
 
   case 0x0e:
     // ld c nn
     cpu.t += 8
     cpu.reg.c = cpu.mmu.Read(cpu.pc + 1)
-    fmt.Printf("0x%0x:\tld c 0x%x\n", n, cpu.mmu.Read(cpu.pc + 1))
+    fmt.Printf("ld c 0x%x", cpu.mmu.Read(cpu.pc + 1))
     cpu.pc += 2
 
   case 0x11:
     // ld de nnnn
     cpu.t += 12
     cpu.reg.Set16("de", uint16(cpu.mmu.ReadWord(cpu.pc + 1)))
-    fmt.Printf("0x%0x:\tld de 0x%x\n", n, cpu.mmu.ReadWord(cpu.pc + 1))
+    fmt.Printf("ld de 0x%x", cpu.mmu.ReadWord(cpu.pc + 1))
     cpu.pc += 3
 
   case 0x1a:
     // ld a,de
     cpu.t += 8
     cpu.reg.a = cpu.mmu.Read(cpu.pc + 1)
-    fmt.Printf("0x%0x:\tld a de\n", n)
+    fmt.Printf("ld a de")
     cpu.pc += 1
 
   case 0x1f:
@@ -106,12 +105,12 @@ func (cpu *CPU) Step(n uint16) {
     c := cpu.reg.GetFlagVal('c')
     cpu.reg.SetFlag('c', cpu.reg.a & 1 == 1)
     cpu.reg.a = cpu.reg.a >> 1 | (c << 7)
-    fmt.Printf("0x%0x:\trra\n", n)
+    fmt.Printf("rra")
     cpu.pc += 1
 
   case 0x20:
     // jr nz nn
-    fmt.Printf("0x%0x:\tjr nz 0x%x\n", n, cpu.mmu.Read(cpu.pc + 1))
+    fmt.Printf("jr nz 0x%x", cpu.mmu.Read(cpu.pc + 1))
     if !cpu.reg.GetFlag('z') {
       cpu.pc += uint16(cpu.mmu.Read(cpu.pc + 1))
       cpu.t += 12
@@ -124,14 +123,14 @@ func (cpu *CPU) Step(n uint16) {
     // ld hl nnnn
     cpu.t += 12
     cpu.reg.Set16("hl", uint16(cpu.mmu.ReadWord(cpu.pc + 1)))
-    fmt.Printf("0x%0x:\tld hl 0x%x\n", n, cpu.mmu.ReadWord(cpu.pc + 1))
+    fmt.Printf("ld hl 0x%x", cpu.mmu.ReadWord(cpu.pc + 1))
     cpu.pc += 3
 
   case 0x22:
     // ldi (hl) hl -- gb sp
     cpu.t += 8
     cpu.mmu.Write(cpu.reg.Get16("hl"), cpu.reg.a)
-    fmt.Printf("0x%0x:\tldi (hl) a\n", n)
+    fmt.Printf("ldi (hl) a")
     cpu.pc += 1
 
   case 0x23:
@@ -139,14 +138,14 @@ func (cpu *CPU) Step(n uint16) {
     cpu.t += 8
     hl := cpu.reg.Get16("hl")
     cpu.reg.Set16("hl", hl+1)
-    fmt.Printf("0x%0x:\thl inc\n", n)
+    fmt.Printf("hl inc")
     cpu.pc += 1
 
   case 0x31:
     // ld sp nnnn
     cpu.t += 12
     cpu.sp = uint16(cpu.mmu.ReadWord(cpu.pc + 1))
-    fmt.Printf("0x%0x:\tld sp 0x%x\n", n, cpu.mmu.ReadWord(cpu.pc + 1))
+    fmt.Printf("ld sp 0x%x", cpu.mmu.ReadWord(cpu.pc + 1))
     cpu.pc += 3
 
   case 0x32:
@@ -155,21 +154,21 @@ func (cpu *CPU) Step(n uint16) {
     hl := cpu.reg.Get16("hl")
     cpu.mmu.Write(hl, cpu.reg.a)
     cpu.reg.Set16("hl", hl - 1)
-    fmt.Printf("0x%0x:\tld (hl-) a\n", n)
+    fmt.Printf("ld (hl-) a")
     cpu.pc += 1
 
   case 0x3e:
     // ld a nn
     cpu.t += 8
     cpu.reg.a = cpu.mmu.Read(cpu.pc + 1)
-    fmt.Printf("0x%0x:\tld a 0x%x\n", n, cpu.mmu.Read(cpu.pc + 1))
+    fmt.Printf("ld a 0x%x", cpu.mmu.Read(cpu.pc + 1))
     cpu.pc += 2
 
   case 0x4f:
     // ld c a
     cpu.t += 4
     cpu.reg.c = cpu.reg.a
-    fmt.Printf("0x%0x:\tld c a\n", n)
+    fmt.Printf("ld c a")
     cpu.pc += 1
 
   case 0x77:
@@ -178,7 +177,7 @@ func (cpu *CPU) Step(n uint16) {
     hl := cpu.reg.Get16("hl")
     cpu.mmu.Write(hl, cpu.reg.a)
     cpu.reg.Set16("hl", hl - 1)
-    fmt.Printf("0x%0x:\tld (hl-) a\n", n)
+    fmt.Printf("ld (hl-) a")
     cpu.pc += 1
 
   case 0x9c:
@@ -193,7 +192,7 @@ func (cpu *CPU) Step(n uint16) {
     cpu.reg.a -=  val
 
     cpu.reg.SetFlag('h', cpu.reg.a & 0x10 == 0x10)
-    fmt.Printf("0x%0x:\tsbc a h\n", n)
+    fmt.Printf("sbc a h")
     cpu.pc += 1
 
   case 0xaf:
@@ -201,7 +200,7 @@ func (cpu *CPU) Step(n uint16) {
     cpu.t += 4
     cpu.reg.a = 0
     cpu.reg.f = 0x80
-    fmt.Printf("0x%0x:\txor a\n", n)
+    fmt.Printf("xor a")
     cpu.pc += 1
 
   case 0xc1:
@@ -209,7 +208,7 @@ func (cpu *CPU) Step(n uint16) {
     cpu.t += 12
     cpu.reg.Set16("bc", cpu.sp)
     cpu.sp += 2
-    fmt.Printf("0x%0x:\tpop bc\n", n)
+    fmt.Printf("pop bc")
     cpu.pc += 1
 
   case 0xcb:
@@ -225,7 +224,7 @@ func (cpu *CPU) Step(n uint16) {
       c := cpu.reg.GetFlagVal('c')
       cpu.reg.SetFlag('c', cpu.reg.c & (1 << 7) >> 7 == 1)
       cpu.reg.c = cpu.reg.c << 1 | c
-      fmt.Printf("0x%0x:\trl c\n", n)
+      fmt.Printf("rl c")
       cpu.pc += 2
 
     case 0x7c:
@@ -235,12 +234,12 @@ func (cpu *CPU) Step(n uint16) {
       cpu.reg.SetFlag('z', bit == 0)
       cpu.reg.SetFlag('n', false)
       cpu.reg.SetFlag('h', false)
-      fmt.Printf("0x%0x:\tbit 7 h\n", n)
+      fmt.Printf("bit 7 h")
       cpu.pc += 2
 
       default:
         // not implemented
-        panic(fmt.Sprintf("0x%0x:\tNI [0xcb%x]\n", n, instr))
+        panic(fmt.Sprintf("NI [0xcb%x]", instr))
     }
 
   case 0xc5:
@@ -248,14 +247,14 @@ func (cpu *CPU) Step(n uint16) {
     cpu.t += 16
     bc := cpu.reg.Get16("bc")
     cpu.mmu.WriteWord(cpu.sp - 2, bc)
-    fmt.Printf("0x%0x:\tpush bc --\n", n)
+    fmt.Printf("push bc --")
     cpu.sp -= 2
     cpu.pc += 1
 
   case 0xc9:
     // ret
     cpu.t += 16
-    fmt.Printf("0x%0x:\tret\n", n)
+    fmt.Printf("ret")
     cpu.pc = uint16(cpu.mmu.ReadWord(cpu.sp))
     cpu.sp += 2
 
@@ -264,7 +263,7 @@ func (cpu *CPU) Step(n uint16) {
     cpu.t += 24
     cpu.mmu.WriteWord(cpu.sp - 2, cpu.pc + 3)
     nn := uint16(cpu.mmu.ReadWord(cpu.pc + 1))
-    fmt.Printf("0x%0x:\tcall 0x%x ==\n", n, nn)
+    fmt.Printf("call 0x%x ==", nn)
     cpu.sp -= 2
     cpu.pc = nn
 
@@ -273,20 +272,22 @@ func (cpu *CPU) Step(n uint16) {
     cpu.t += 8
     nn := uint16(cpu.mmu.Read(cpu.pc + 1))
     cpu.mmu.Write(0xff00 + nn, cpu.reg.a)
-    fmt.Printf("0x%0x:\tld (0xff00 + 0x%x) a\n", n, nn)
+    fmt.Printf("ld (0xff00 + 0x%x) a", nn)
     cpu.pc += 2
 
   case 0xe2:
     // ld (FF00 + c) a -- gb sp
     cpu.t += 8
     cpu.mmu.Write(0xff00 + uint16(cpu.reg.c), cpu.reg.a)
-    fmt.Printf("0x%0x:\tld (0xff00 + c) a\n", n)
+    fmt.Printf("ld (0xff00 + c) a")
     cpu.pc += 1
 
   default:
     // not implemented
-    panic(fmt.Sprintf("0x%0x:\tNI [0x%x]\n", n, opcode))
-    // fmt.Printf("0x%0x:\tNI [0x%x]\n", n, opcode)
+    panic(fmt.Sprintf("NI [0x%x]\n", opcode))
+    // fmt.Printf("NI [0x%x]", opcode)
     cpu.pc += 1
   }
+
+  fmt.Printf("\n")
 }
